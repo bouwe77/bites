11f32fd0875a2ea6fd22d1f93ffda2cfea59ca09
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-06-16 06:41:00 +0200

    feat: much more nice UI components

--- src/bites/XState coffee machine/index.js
+++ src/bites/XState coffee machine/index.js
@@ -1,26 +1,29 @@
 import { useMachine } from '@xstate/react'
 import coffeeMachine from './coffeeMachine'
-import { CoffeeMachine, Display, PowerButton } from './components'
+import { CoffeeButton, CoffeeButtons, CoffeeMachine, Display, PowerButton } from './components'
 
 const App = () => {
   const [state, send] = useMachine(coffeeMachine)
+
+  const isOn = ['starting', 'on', 'stopping'].some(state.matches)
 
   return (
     <>
       <h1>My Coffee Machine</h1>
 
       <CoffeeMachine>
-        <div>
-          <PowerButton
-            isOn={['starting', 'on', 'stopping'].some(state.matches)}
-            toggle={() => send('TOGGLE_POWER')}
-          />
+        <PowerButton isOn={isOn} toggle={() => send('TOGGLE_POWER')} />
 
-          <Display>
-            {['starting', 'stopping'].some(state.matches) ? 'Please wait...' : null}
-            {state.matches('on') ? 'What can I get you?' : null}
-          </Display>
-        </div>
+        <Display isOn={isOn}>
+          {['starting', 'stopping'].some(state.matches) ? 'Please wait...' : null}
+          {state.matches('on') ? 'What can I get you?' : null}
+        </Display>
+
+        <CoffeeButtons>
+          <CoffeeButton disabled={!state.matches('on')}>Ristretto</CoffeeButton>
+          <CoffeeButton disabled={!state.matches('on')}>Espresso</CoffeeButton>
+          <CoffeeButton disabled={!state.matches('on')}>Lungo</CoffeeButton>
+        </CoffeeButtons>
       </CoffeeMachine>
 
       <div style={{ border: '3px dashed pink', padding: '3px', margin: '10px' }}>
